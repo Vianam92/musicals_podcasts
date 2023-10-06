3c2e5c2f4e97ab105f65e16f040acb17aac519e9
Mariana Viana <mariana.viana@kairosds.com>
2023-10-06 13:39:36 +0200

    feat: add views

---
+++ src/views/character.podcast-page.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+const CharacterPodcast = () => {
+  useEffect(() => {
+
+  })
+    return (
+      <div>
+        <h1>Hola, Bienvenido al sistema</h1>
+      </div>
+    );
+}
+
+export default CharacterPodcast;
---
+++ src/views/podcast.detail-page.tsx
@@ -0,0 +1,6 @@
+const PodcastDetail = () => {
+
+  return <h1>Hola</h1>;
+};
+
+export default PodcastDetail;
