8ac527850ace0adf9b92b0697b365805c72a9748
Mariana Viana <mariana.viana@kairosds.com>
2023-10-07 03:14:38 +0200

    style: filter component

--- src/pods/filter/filter.podacast.tsx
+++ src/pods/filter/filter.podacast.tsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import React from "react";
+import "./filter.css";
+import { Title, TitleH2, InputSearch } from '../../styled/styled.component';
 
 interface Props {
     podcastSearch: string;
@@ -14,9 +16,9 @@ const FilterPodcast = ({ handlerInput, podcastSearch }: Props) => {
   };
 
   return (
-    <section>
-      <h2>100</h2>
-      <input
+    <section className='search-section'>
+      <TitleH2>100</TitleH2>
+      <InputSearch
         type="text"
         value={podcastSearch}
         placeholder="Filter podcast"
--- src/styled/styled.component.tsx
+++ src/styled/styled.component.tsx
@@ -12,3 +12,22 @@ export const HeaderStyled = styled.header`
 export const Title = styled.h1`
   color: rgb(12, 91, 128); 
 `;
+
+export const TitleH2 = styled.h2`
+  color: white;
+  background: rgb(12, 91, 128);
+  text-align: center;
+  margin:10px;
+  padding:10px;
+  border-radius:15px;
+`;
+
+export const InputSearch = styled.input`
+height:30px;
+padding: 20px 8px;
+border-radius:5px;
+border: solid 2px hsla(148, 7%, 42%, 0.293);
+font-size:17px;
+width:250px;
+
+`
